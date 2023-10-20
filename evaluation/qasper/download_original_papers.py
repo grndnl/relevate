fc892cf3fad921fcd8b57a8e38fd1384452ebe60
Daniele Grandi <daniele.grandi@autodesk.com>
2023-10-19 21:51:48 -0700

    fixes

--- evaluation/qasper/download_original_papers.py
+++ evaluation/qasper/download_original_papers.py
@@ -12,43 +12,51 @@ def get_all_names(directory, pattern):
     return [address.stem[:10] for address in files]
 
 
-def download_papers(papers, split, downloaded, filetypes):
-    for filetype in filetypes:
-        for paper in tqdm(papers, desc=f"Downloading {split} papers"):
-            if paper not in downloaded:
+def download_papers(papers, split, downloaded, filetype):
+    for paper in tqdm(papers, desc=f"Downloading {split} papers as {filetype}"):
+        if paper not in downloaded:
+            if filetype == 'pdf':
+                path = Path('dataset', split, paper + '.pdf')
+                os.makedirs(path.parent, exist_ok=True)
+            elif filetype == 'src':
+                path = Path('dataset', 'source_zip', split, paper + '.tar.gz')
+                os.makedirs(path.parent, exist_ok=True)
+            elif filetype == 'html':
+                path = Path('dataset', 'html', split, paper + '.html')
+                os.makedirs(path.parent, exist_ok=True)
+            else:
+                raise Exception(f"Filetype not supported: {filetype}")
+            try:
                 if filetype == 'pdf':
-                    path = Path('dataset', split, paper + '.pdf')
-                    os.makedirs(path.parent, exist_ok=True)
+                    written_path, _ = urlretrieve(f"https://export.arxiv.org/pdf/{paper}.pdf", path)
                 elif filetype == 'src':
-                    path = Path('dataset', 'source', split, paper + '.tar.gz')
-                    os.makedirs(path.parent, exist_ok=True)
-                else:
-                    raise f"Filetype not supported: {filetype}"
-                try:
-                    if filetype == 'pdf':
-                        written_path, _ = urlretrieve(f"https://export.arxiv.org/pdf/{paper}.pdf", path)
-                    elif filetype == 'src':
-                        written_path, _ = urlretrieve(f"https://export.arxiv.org/src/{paper}", path)
-                except urllib.error.URLError as e:
-                    print(e)
-                    print(paper)
-                time.sleep(16)
+                    written_path, _ = urlretrieve(f"https://export.arxiv.org/src/{paper}", path)
+                elif filetype == 'html':
+                    written_path, _ = urlretrieve(f"https://ar5iv.labs.arxiv.org/html/{paper}", path)
+            except urllib.error.URLError as e:
+                print(e)
+                print(paper)
+            time.sleep(16)
     return
 
 
-def main(filetype):
-    for split in ['train', 'dev', 'test']:
-        qasper_papers = pd.read_json(f"dataset/qasper-{split}-v0.3.json", convert_axes=False).transpose()
-        if filetype == 'pdf':
-            downloaded = get_all_names(split, "*.pdf")
-        elif filetype == 'src':
-            downloaded = get_all_names('src' + split, "*")
-        else:
-            raise f"Filetype not supported: {filetype}"
-        papers = qasper_papers.index.to_list()
-        download_papers(papers, split, downloaded, filetype)
+def main(filetypes):
+    for filetype in filetypes:
+        for split in ['train', 'dev', 'test']:
+            qasper_papers = pd.read_json(f"dataset/processed_original/qasper-{split}-v0.3.json", convert_axes=False).transpose()
+            if filetype == 'pdf':
+                downloaded = get_all_names('pdf/' + split, "*.pdf")
+            elif filetype == 'src':
+                downloaded = get_all_names('source_zip/' + split, "*")
+            elif filetype == 'html':
+                downloaded = get_all_names('html/' + split, "*")
+            else:
+                raise Exception(f"Filetype not supported: {filetype}")
+            papers = qasper_papers.index.to_list()
+            download_papers(papers, split, downloaded, filetype)
 
 
 if __name__ == '__main__':
-    # main(filetype=['pdf'])
-    main(filetype=['src'])
+    # main(['pdf'])
+    # main(['src'])
+    main(['html'])
